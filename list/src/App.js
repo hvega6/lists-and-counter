4738c16fc5b2305c7a0568a27d7b0f48227ac37d
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2023-10-05 15:04:51 -0700

    used routes from npm

---
+++ list/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import AboutMe from "./aboutme"
+import Homepage from "./HomePage"
+import {Routes, Route, Link} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="navbar-div">
+      <nav className="nav">
+        <Link to="/" className='nav-item'>Homepage</Link>
+        <Link to="/about-me" className='nav-item'>About Me</Link>
+      </nav>
+      <Routes>
+      <Route path='/' element={<Homepage/>}/>
+      <Route path='/about-me' element={<AboutMe/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
